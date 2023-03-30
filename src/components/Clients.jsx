97db9f207670bed60b624577f41b641ac34fb48f
Daniel Taccola <danieltaccola@yahoo.com.br>
2023-03-30 19:42:19 -0300

    arrow function const components

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Clients() {
+const Clients = () => {
   return (
     <div>Clients</div>
   )
--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function FeedbackCard() {
+const FeedbackCard = () => {
   return (
     <div>FeedbackCard</div>
   )
